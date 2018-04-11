469fe49f1fc9c1b8a6a01a442345f99796e11833
nabilmakarem <38248174+nabilmakarem@users.noreply.github.com>
2018-04-11 14:23:39 +0200

    Add files via upload

---
+++ myheader.h
@@ -0,0 +1,18 @@
+#ifndef MYHEADER_H_INCLUDED
+#define MYHEADER_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+
+#define TXTLEN 4096
+
+#endif
